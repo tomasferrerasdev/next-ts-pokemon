0d2393ad373d7a76310607b53942fc70b1ff58e9
Tomas Ferreras <hellotomasdev@gmail.com>
2022-06-21 15:25:42 -0300

    clickable cards to go to the pokemon screen details

--- components/pokemon/FavoriteCardPokemon.tsx
+++ components/pokemon/FavoriteCardPokemon.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { useRouter } from "next/router";
 import { Grid, Card } from "@nextui-org/react";
 
 interface Props {
@@ -6,8 +7,13 @@ interface Props {
 }
 
 export const FavoriteCardPokemon: FC<Props> = ({ pokemonID }) => {
+  const router = useRouter();
+  const onFavoriteClick = () => {
+    router.push(`/pokemon/${pokemonID}`);
+  };
+
   return (
-    <Grid key={pokemonID} xs={6} sm={3} md={2} xl={1}>
+    <Grid key={pokemonID} xs={6} sm={3} md={2} xl={1} onClick={onFavoriteClick}>
       <Card hoverable clickable css={{ padding: 10 }}>
         <Card.Image
           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonID}.svg`}
